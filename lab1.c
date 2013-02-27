3bf7fcffbd4e94f0ea4e8ec58f2c60dfe2ed2d88
kungurov denis <ridik18.03@mail.ru>
2013-02-27 17:16:21 +0300

    first lines of code

--- lab1.c
+++ lab1.c
@@ -1,6 +1,49 @@
 #include<stdio.h>
 #include<conio.h>
 #include<malloc.h>
+struct firm
+{
+    char im[30];
+    char nalog[7];
+    char *date;
+    char *date_fact;
+}   arr[50];
+int mystrcmp(char *p,char *q)
+{
+    int i=0,ind=0;
+    while(p[i] && q[i])
+    {
+        if (p[i]==q[i]) ind=1;
+        else ind=0;
+        if ((!p[i+1] && q[i+1]) || (p[i+1] && !q[i+1])) ind = 0;
+        i++;
+    }
+    return ind;
+}
+void set_name()
+{
+    int i;
+    char *p;
+    for(i = 0; i < 50; i++)
+    {
+        gets(p);
+        if (mystrcmp(p,"end")) break;
 
-struct person { char im[20], fam[30], otch[40];
-                };
+    }
+
+}
+void interact_firms ()
+{
+    int i;
+    for(i = 0; i < 50; i++)
+        puts(arr[i].im);
+}
+int main()
+{
+
+    set_name();
+    interact_firms();
+    getch ();
+    return 0;
+
+}
