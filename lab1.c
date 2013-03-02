aeabfca01b4d4052eb71b6b83663e3d58de24a7a
kungurov denis <ridik18.03@mail.ru>
2013-03-02 10:55:39 +0300

    Fixed position of the structures and the problem of global peremennyh.Takzhe fixes minor problems output.

--- lab1.c
+++ lab1.c
@@ -3,16 +3,27 @@
 #include<malloc.h>
 // VARIANT 5
 typedef enum {JAN,FEB,MAR,APR,MAY,JUN,JUL,AUG,SEP,OCT,NOV,DEC,NONE} month;
-char MONTH[13][5]={"JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC","NONE"};
-struct firm;
-struct date;
+struct date
+    {
+        int d;
+        month m;
+        int y;
+    };
+    struct firm
+    {
+        char name[30];
+        char tax[7];
+        struct date last_date;
+        struct date act_date;
+    };
+
 int mystrcmp(char *p,char *q)
 {
     int i=0,ind=0;
     while(p[i] && q[i])
     {
         if (p[i]==q[i]) ind=1;
-        else ind=0;
+        else return 0;
         if ((!p[i+1] && q[i+1]) || (p[i+1] && !q[i+1])) ind = 0;
         i++;
     }
@@ -26,40 +37,23 @@ void mystrcpy(char *p,char *q)
         p[i]=q[i];
         i++;
     }
+	p[i]='\0';
 }
-void char_to_enum1(char *p,int i,struct firm arr)
+month char_to_enum1(char *p)
 {
-
-    if     ( mystrcmp(p, "jan") ) {arr[i].last_date.m=JAN;}
-    else if( mystrcmp(p, "feb") ) {arr[i].last_date.m=FEB;}
-    else if( mystrcmp(p, "mar") ) {arr[i].last_date.m=MAR;}
-    else if( mystrcmp(p, "apr") ) {arr[i].last_date.m=APR;}
-    else if( mystrcmp(p, "may") ) {arr[i].last_date.m=MAY;}
-    else if( mystrcmp(p, "jun") ) {arr[i].last_date.m=JUN;}
-    else if( mystrcmp(p, "jul") ) {arr[i].last_date.m=JUL;}
-    else if( mystrcmp(p, "aug") ) {arr[i].last_date.m=AUG;}
-    else if( mystrcmp(p, "sep") ) {arr[i].last_date.m=SEP;}
-    else if( mystrcmp(p, "okt") ) {arr[i].last_date.m=OCT;}
-    else if( mystrcmp(p, "nov") ) {arr[i].last_date.m=NOV;}
-    else if( mystrcmp(p, "dec") ) {arr[i].last_date.m=DEC;}
-    else if( mystrcmp(p, "0"  ) ) {arr[i].last_date.m=NONE;}
-}
-void char_to_enum2(char *p,int i,struct firm arr)
-{
-
-    if     ( mystrcmp(p, "jan") ) {arr[i].act_date.m=JAN;}
-    else if( mystrcmp(p, "feb") ) {arr[i].act_date.m=FEB;}
-    else if( mystrcmp(p, "mar") ) {arr[i].act_date.m=MAR;}
-    else if( mystrcmp(p, "apr") ) {arr[i].act_date.m=APR;}
-    else if( mystrcmp(p, "may") ) {arr[i].act_date.m=MAY;}
-    else if( mystrcmp(p, "jun") ) {arr[i].act_date.m=JUN;}
-    else if( mystrcmp(p, "jul") ) {arr[i].act_date.m=JUL;}
-    else if( mystrcmp(p, "aug") ) {arr[i].act_date.m=AUG;}
-    else if( mystrcmp(p, "sep") ) {arr[i].act_date.m=SEP;}
-    else if( mystrcmp(p, "okt") ) {arr[i].act_date.m=OCT;}
-    else if( mystrcmp(p, "nov") ) {arr[i].act_date.m=NOV;}
-    else if( mystrcmp(p, "dec") ) {arr[i].act_date.m=DEC;}
-    else if( mystrcmp(p, "0"  ) ) {arr[i].act_date.m=NONE;}
+    if     ( mystrcmp(p, "jan") ) {return JAN;}
+    else if( mystrcmp(p, "feb") ) {return FEB;}
+    else if( mystrcmp(p, "mar") ) {return MAR;}
+    else if( mystrcmp(p, "apr") ) {return APR;}
+    else if( mystrcmp(p, "may") ) {return MAY;}
+    else if( mystrcmp(p, "jun") ) {return JUN;}
+    else if( mystrcmp(p, "jul") ) {return JUL;}
+    else if( mystrcmp(p, "aug") ) {return AUG;}
+    else if( mystrcmp(p, "sep") ) {return SEP;}
+    else if( mystrcmp(p, "oct") ) {return OCT;}
+    else if( mystrcmp(p, "nov") ) {return NOV;}
+    else if( mystrcmp(p, "dec") ) {return DEC;}
+    else if( mystrcmp(p, "0"  ) ) {return NONE;}
 }
 int myatoi(char q[])                   // в функцию передается указатель
 {
@@ -72,7 +66,7 @@ int myatoi(char q[])                   // в функцию передается указатель
     n=10*n+(*(q+i)-'0');
     return znak*n;                                               // возврат signed int
 }
-int debt(int i,struct firm arr)
+int debt(int i,struct firm arr[50])
 {
     if(arr[i].act_date.y>arr[i].last_date.y) return 1;
     else if(arr[i].act_date.y<arr[i].last_date.y) return 0;
@@ -82,35 +76,35 @@ int debt(int i,struct firm arr)
     else if(arr[i].act_date.d<arr[i].last_date.d) return 0;
     return 0;
 }
-void set_name(struct firm arr)
+void set_name(struct firm arr[50])
 {
     int i;
     char *p;
     for(i = 0; i < 50; i++)
     {
-        p=(char*)malloc(30);
+        p=(char*)calloc(30,1);
         printf("Please enter company name Numb.%d:",i+1);
         gets(p);
-        if (mystrcmp(p,"end")) break;
+        if (mystrcmp(p,"end")) {arr[i].name[0]=NULL; break;}
         else mystrcpy(arr[i].name,p);
         free(p);
     }
 }
-void set_tax(struct firm arr)
+void set_tax(struct firm arr[50])
 {
     int i;
-    for(i = 0; i < 50 && arr[i].name[1]; i++)
+    for(i = 0; i < 50 && arr[i].name[0]; i++)
     {
         printf("Please enter amount of tax to the firm %s:",arr[i].name);
         fflush(stdin);
         scanf("%s",arr[i].tax);
     }
 }
-void set_dates(struct firm arr)
+void set_dates(struct firm arr[50])
 {
 
     int i;
-    for(i = 0; i < 50 && arr[i].name[1]; i++)
+    for(i = 0; i < 50 && arr[i].name[0]; i++)
     {
         char *p;
         p=(char*)calloc(4,1);
@@ -118,21 +112,21 @@ void set_dates(struct firm arr)
         printf("DD MMM YYYY:");
         fflush(stdin);
 	scanf("%d%s%d",&arr[i].last_date.d,p,&arr[i].last_date.y);
-	char_to_enum1(p,i,&arr);
+	arr[i].last_date.m=char_to_enum1(p);
 	free(p);
 	p=(char*)malloc(4);
 	printf("Please enter date of the actual tax payment (or '0 'in all respects, if not been made) for the firm %s\n",arr[i].name);
 	printf("DD MMM YYYY:");
 	fflush(stdin);
 	scanf("%d%s%d",&arr[i].act_date.d,p,&arr[i].act_date.y);
-	char_to_enum2(p,i,&arr);
+	arr[i].act_date.m=char_to_enum1(p);
 	free(p);
     }
 }
-void display_firms_data (struct firm arr)
+void display_firms_data (struct firm arr[50], char MONTH[13][5])
 {
     int i;
-    for(i = 0; i < 50 && arr[i].name[1]; i++)
+    for(i = 0; i < 50 && arr[i].name[0]; i++)
     {
         printf("Information about the firm %s:\n",arr[i].name);
         printf("The value of tax - %s\n",arr[i].tax);
@@ -142,28 +136,17 @@ void display_firms_data (struct firm arr)
 }
 int main()
 {
-    struct date
-    {
-        int d;
-        month m;
-        int y;
-    };
-    struct firm
-    {
-        char name[30];
-        char tax[7];
-        struct date last_date;
-        struct date act_date;
-    }   arr[50];
+	char MONTH[13][5]={"JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC","NONE"};
+    struct firm arr[50];
     int i,j;
     struct firm tmp;
-    set_name(&arr);
-    set_tax(&arr);
-    set_dates(&arr);
+    set_name(arr);
+    set_tax(arr);
+    set_dates(arr);
     //display_firms_data();
-    for(i=0;i<49;i++)
+    for(i=0;i<49 && arr[i].name[0];i++)
     {
-        for(j=i+1;j<50;j++)
+        for(j=i+1;j<50 && arr[j].name[0];j++)
 	    {
             if (myatoi(arr[i].tax)<myatoi(arr[j].tax))
             {
@@ -179,7 +162,7 @@ int main()
     //    else
     //}
     //if(debt(i)) printf("%s has a debt to pay tax value %s$",arr[i].name,arr[i].tax);
-    display_firms_data();
+    display_firms_data(arr,&MONTH);
     getch();
     return 0;
 }
