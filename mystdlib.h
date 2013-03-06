0e1fa9e988028fd3aed8c69d1c5e6435157723a2
kungurov denis <ridik18.03@mail.ru>
2013-03-07 00:09:17 +0300

    fix file

--- mystdlib.h
+++ mystdlib.h
@@ -1,8 +1,8 @@
 #ifndef MYSTDLIB_H_INCLUDED
 #define MYSTDLIB_H_INCLUDED
 
-int mystrcmp(char *str1,char *str2);
-void mystrcpy(char *str1,char *str2);
+int mystrcmp(char *str1, char *str2);
+void mystrcpy(char *str1, char *str2);
 int myatoi(char *str);
 
-#endif // MYSTDLIB_H_INCLUDED
+#endif                          // MYSTDLIB_H_INCLUDED
