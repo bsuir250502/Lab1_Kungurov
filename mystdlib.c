4eeadf0661216d12442bf90f0efc02cf2855a9f8
kungurov denis <ridik18.03@mail.ru>
2013-03-07 00:09:09 +0300

    fix file

--- mystdlib.c
+++ mystdlib.c
@@ -1,31 +1,35 @@
 #define MYSTDLIB_H_INCLUDED
 #include "mystdlib.h"
-int mystrcmp(char *str1,char *str2)
+int mystrcmp(char *str1, char *str2)
 {
-    while( *str1 && *str2 && (*str1 == *str2) ){
-    str1++;
-    str2++;
+    while (*str1 && *str2 && (*str1 == *str2)) {
+        str1++;
+        str2++;
     };
     return *str1 - *str2;
 }
-void mystrcpy(char *str1,char *str2)
+
+void mystrcpy(char *str1, char *str2)
 {
-    int i=0;
-    while(str2[i])
-    {
-        str1[i]=str2[i];
+    int i = 0;
+    while (str2[i]) {
+        str1[i] = str2[i];
         i++;
     }
-    str1[i]='\0';
+    str1[i] = '\0';
 }
+
 int myatoi(char *str)
 {
-    int n,i=0,sign;
-    while(*(str+i)==' ') i++;
-    if (!*(str+i)) return 0;
-    sign=(*(str+i)=='-')? -1: 1;
-    if (*(str+i)=='+' || *(str+i)=='-') i++;
-    for(n=0; *(str+i)>='0' && *(str+i)<='9'; i++)
-    n=10*n+(*(str+i)-'0');
-    return sign*n;
+    int n, i = 0, sign;
+    while (*(str + i) == ' ')
+        i++;
+    if (!*(str + i))
+        return 0;
+    sign = (*(str + i) == '-') ? -1 : 1;
+    if (*(str + i) == '+' || *(str + i) == '-')
+        i++;
+    for (n = 0; *(str + i) >= '0' && *(str + i) <= '9'; i++)
+        n = 10 * n + (*(str + i) - '0');
+    return sign * n;
 }
