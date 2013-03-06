768252e2872fe6f7ecef25909d3e2d643882bbcc
kungurov denis <ridik18.03@mail.ru>
2013-03-06 18:26:53 +0300

    fix file

--- mystdlib.c
+++ mystdlib.c
@@ -1,12 +1,9 @@
-
 #define MYSTDLIB_H_INCLUDED
-
+#include "mystdlib.h"
 int mystrcmp(char *str1,char *str2)
 {
     int i=0,equal_word=0;
-    while(str1[i]) i++;
-    str1[i-1]='\0';
-    i=0;
+  
     while(str1[i] && str2[i])
     {
         if (str1[i]==str2[i]) equal_word=1;
