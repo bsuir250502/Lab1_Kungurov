b1b151115a635709d2e2607223db5174bdd0c747
kungurov denis <ridik18.03@mail.ru>
2013-03-06 13:37:59 +0300

    changed mystdlib.h

--- mystdlib.h
+++ mystdlib.h
@@ -1,8 +1,9 @@
 #ifndef MYSTDLIB_H_INCLUDED
 #define MYSTDLIB_H_INCLUDED
+#include "mystdlib.c"
 
 int mystrcmp(char *str1,char *str2);
 void mystrcpy(char *str1,char *str2);
-int myatoi(char str[]);
+int myatoi(char *str);
 
 #endif // MYSTDLIB_H_INCLUDED
