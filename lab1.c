889f916b51c4e3b84a6d91587e57f2c23150f78f
kungurov denis <ridik18.03@mail.ru>
2013-03-13 19:06:46 +0300

    fix code

--- lab1.c
+++ lab1.c
@@ -89,7 +89,7 @@ void set_dates(struct company *arr)
                 break;
             } else {
                 arr[i].is_debt = 0;
-                *(input_buffer + strlen(str) - 1) = '\0';
+                *(input_buffer + strlen(input_buffer) - 1) = '\0';
                 if (!strptime(input_buffer, "%d %b %y", &tm))
                     printf("Enter correct date");
                 else
